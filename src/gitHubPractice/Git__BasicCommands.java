571b25614c0a6ae62e5e19091b56a7ffd33b24dc
fromspacepa <97071663+fromspacepa@users.noreply.github.com>
2023-07-19 14:57:08 -0400

    Update Git__BasicCommands.java

--- src/gitHubPractice/Git__BasicCommands.java
+++ src/gitHubPractice/Git__BasicCommands.java
@@ -5,6 +5,8 @@ public class Git__BasicCommands {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Here we are going to learn about git and gitHub Repos");
+
 	}
 
 }
